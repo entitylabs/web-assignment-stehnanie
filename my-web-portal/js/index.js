7806e64d12fe2215e312f2fa3794c641cc95d963
Sanyam Goel <entitylabs@gmail.com>
2024-05-21 19:07:17 +0200

    added script

--- my-web-portal/js/index.js
+++ my-web-portal/js/index.js
@@ -1,5 +1,12 @@
-const portal_title= 'Stephanie Music Gallery';
+/** usage of constant and temolate literal. 
+*Template literal helps in web portal main title in multiple lines
+The value is also constant, to ensure its immutable.
+*/ 
+const portal_title= `Stephanie Music 
+                     gallery`;
 
+// selecting an element
 const home_label=document.getElementById('home-label');
 
+// modifying an element data
 home_label.textContent=portal_title;
