c21e73220d45c0f9ce2785eec69b6c2853e2615d
Sanyam Goel <entitylabs@gmail.com>
2024-05-21 22:35:15 +0200

    updated form

---
+++ my-web-portal/js/contact.js
@@ -0,0 +1,73 @@
+function onSubmit() {
+
+
+    const nameElement = document.getElementById('name');
+
+    const isNameFieldvalid = validateField(nameElement);
+
+    const phoneElement = document.getElementById('phone');
+
+    const isPhoneFieldvalid = validateField(phoneElement);
+
+    const messageElement = document.getElementById('message');
+
+    const isMesssageFieldvalid = validateField(messageElement);
+
+    const isFormValid = isNameFieldvalid && isPhoneFieldvalid && isMesssageFieldvalid;
+
+    onvalidFormSubmission(isFormValid, nameElement, phoneElement, messageElement);
+
+}
+
+
+function onvalidFormSubmission(isFormValid, nameElement, phoneElement, messageElement) {
+
+
+    if (isFormValid) {
+
+        // clear fields
+        nameElement.value = '';
+
+        phoneElement.value = '';
+
+        messageElement.value = '';
+
+        // set success label
+        const infoField = document.getElementById('info_label');
+
+        infoField.textContent = "Message sent successfully!";
+
+        infoField.setAttribute('class', 'success');
+
+        // unset success label after 2 seconds
+        setInterval(function () {
+
+            infoField.removeAttribute('class', 'success');
+
+            infoField.textContent = '';
+
+        }, 2 * 1000);
+    }
+}
+
+
+
+function validateField(field) {
+
+    if (field.value == '') {
+
+        field.setAttribute('class', 'error-input')
+
+        setInterval(function () {
+
+            field.removeAttribute('class', 'error-input')
+
+        }, 2 * 1000);
+
+        return false;
+
+    }
+
+    return true;
+
+}
